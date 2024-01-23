88275d4296781aa0516235b6746875d91a4e62f4
Luiz Humphries <luizgustavo.frota@outlook.com>
2024-01-23 12:51:42 -0300

    Columns for db created

--- models/user.py
+++ models/user.py
@@ -1,13 +1,11 @@
-from app import db
 from flask_login import UserMixin
+from database import db
 
 class User(db.Model, UserMixin):
+    """ Default model for User in the db """
     # model cfg
     # id: int, username: string, password: string
 
     id = db.Column(db.Integer, primary_key=True) #unique key
     username = db.Column(db.String(80), nullable=False, unique=True) #unique username
     password = db.Column(db.String(80), nullable=False)
-
-    
-
